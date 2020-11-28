b2052d49a21a1b40387516c4d5ef7825d5303791
Ian Bautista <victor.jackal@gmail.com>
2020-11-29 01:43:39 +0800

    Add unit tests

--- src/test/java/com/restapi/AppTest.java
+++ src/test/java/com/restapi/AppTest.java
@@ -1,20 +1,48 @@
 package com.restapi;
 
-import static org.junit.Assert.assertTrue;
+import java.io.File;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void urlListValidForZip() {
+        Unirest.setTimeouts(0, 0);
+        try {
+            HttpResponse<String> response = Unirest.post("localhost:4200/api/upload")
+                    .field("file", new File("/D:/Dev/angular/StudentAssets/ApiImages/images/products/products.zip"))
+                    .asString();
+
+            String body = response.getBody();
+            String[] urls = body.split(",");
+            assertTrue(urls.length > 0);
+        } catch (Exception e) {
+            assertTrue(false);
+        }
+    }
+
+    public void urlListValidForSingleFile() {
+        Unirest.setTimeouts(0, 0);
+        try {
+            HttpResponse<String> response = Unirest.post("localhost:4200/api/upload")
+                    .field("file", new File("/D:/Dev/angular/AutoParts/client/src/assets/images/hero1.jpg")).asString();
+
+            String body = response.getBody();
+            String[] urls = body.split(",");
+            assertEquals(urls.length, 1);
+        } catch (Exception e) {
+            assertTrue(false);
+        }
     }
 }
