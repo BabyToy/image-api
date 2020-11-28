f0f6c4713bc5d988fa8bbdac9d3c84b5a09d8967
Ian Bautista <victor.gmal.com>
2020-11-28 18:51:33 +0800

    Implement upload endpoint

--- src/main/java/com/restapi/route/ImageRoute.java
+++ src/main/java/com/restapi/route/ImageRoute.java
@@ -1,21 +1,59 @@
 package com.restapi.route;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.FilenameUtils;
+
 import spark.Spark;
 
 public class ImageRoute {
 
-    public void ImageRoute() {
+    private File storageDir;
+
+    public ImageRoute() {
+        this.storageDir = new File("storage");
+        System.out.println("Path: " + this.storageDir.getAbsolutePath());
+        if (!this.storageDir.isDirectory()) {
+            this.storageDir.mkdir();
+        }
     }
 
     public void route() {
-      Spark.port(4200);
+        Spark.port(4200);
 
-      Spark.get("/api/image", (request, response) -> {
+        Spark.get("/api/image", "multipart/form-data", (request, response) -> {
             return "Return from GET";
         });
 
+        long limits = 100000000;
         Spark.post("/api/upload", (request, response) -> {
-            return "Return from POST";
+            String base = "localhost:4200/api/image";
+            UUID uuid = UUID.randomUUID();
+            String id = uuid.toString();
+
+            MultipartConfigElement multipartConfigElement = new MultipartConfigElement(storageDir.getName(), limits, limits, 1024);
+            request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
+
+            Part fileUpload = request.raw().getPart("file");
+            String fileName = fileUpload.getSubmittedFileName();
+            String extension =  FilenameUtils.getExtension(fileName);
+            Path target = Paths.get(this.storageDir + "/" + id + "." + extension);
+            try (final InputStream streamIn = fileUpload.getInputStream()) {
+                Files.copy(streamIn, target);
+                fileUpload.delete();
+            }
+            multipartConfigElement = null;
+            fileUpload = null;
+
+            return base + "?id=" + id;
         });
     }
 }
